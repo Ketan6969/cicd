9d7eba7c48df16971da9e759012a6804641b7720
Ketan6969 <solankiketan156@gmail.com>
2024-06-28 22:12:17 +0530

    changed stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Get the Docker container ID
-ContainerID=$(docker ps -q)
+echo "Hello"
+# # Get the Docker container ID
+# ContainerID=$(docker ps -q)
 
-# Check if a container ID was found
-if [ -z "$ContainerID" ]; then
-    echo "No running Docker container found."
-else
-    echo "Removing Docker container with ID: $ContainerID"
-    sudo docker rm -f $ContainerID
-fi
+# # Check if a container ID was found
+# if [ -z "$ContainerID" ]; then
+#     echo "No running Docker container found."
+# else
+#     echo "Removing Docker container with ID: $ContainerID"
+#     sudo docker rm -f $ContainerID
+# fi
