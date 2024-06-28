ca804a5de1003cd20e1c6c0d4bb8cc038531cd9b
Ketan6969 <solankiketan156@gmail.com>
2024-06-28 22:31:17 +0530

    Made some changes

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-echo "Hello"
-# # Get the Docker container ID
-# ContainerID=$(docker ps -q)
-
-# # Check if a container ID was found
-# if [ -z "$ContainerID" ]; then
-#     echo "No running Docker container found."
-# else
-#     echo "Removing Docker container with ID: $ContainerID"
-#     sudo docker rm -f $ContainerID
-# fi
+ContainerID = `docker ps | awk -F"" {print$1}` 
+docker rm -f $ContainerID
