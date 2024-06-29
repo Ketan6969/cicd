6f7f246b023853005dee0ac061d2b0d3d5677521
Ketan6969 <solankiketan156@gmail.com>
2024-06-30 00:25:58 +0530

    na

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask("__name__")
 
 @app.route("/")
 def home():
-    return f"</h3>Hello from Meowww To CI/CD!!! </h3>"
+    return f"</h3>This is CI/CD!!!!</h3>"
 
 if __name__ == "__main__":
     app.run(debug=True,
