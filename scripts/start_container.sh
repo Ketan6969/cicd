d1b650fa7d2542068fe2f562be7c26cc85a64e2e
Ketan6969 <solankiketan156@gmail.com>
2024-06-30 00:16:39 +0530

    N/A

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -8,4 +8,6 @@ docker pull ketan2004/sample-python-flask-app
 #run the docker image as an container 
 echo "starting the Flask app"
 docker run -d -p 5000:5000 ketan2004/sample-python-flask-app
+echo "Printing the ContainerID: "
+docker ps
 
