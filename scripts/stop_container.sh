41f8102cf4e50bc2ae143f722fadc1bb8fb4b6e1
Ketan6969 <solankiketan156@gmail.com>
2024-06-29 23:48:03 +0530

    Updated the stop container command

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-ContainerID = `docker ps | awk -F"" {print$1}` 
-docker rm -f $ContainerID
+#!/bin/bash
+
+# Fetch the name of the running docker container
+container_name=$(docker ps --format "{{.Names}}")
+
+# Check if any running container exists
+if [ -n "$container_name" ]; then
+    # Remove the running container
+    docker rm -f $container_name
+    echo "Container $container_name removed successfully."
+else
+    echo "No running container found."
+fi
